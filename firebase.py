61767f5c34a168f4a1987887647020d909be367f
JWillian00 <jnmoura.jonathan@braveo.com.br>
2025-02-14 15:27:09 -0300

    Update firebase.py

--- firebase.py
+++ firebase.py
@@ -6,6 +6,7 @@ from consulta_status_chamado import verificar_status_chamado
 from datetime import datetime
 import pytz
 import json
+import os
 
 
 firebase_config = json.loads(os.getenv("FIREBASE_CREDENTIALS"))
