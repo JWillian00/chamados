da1e21b46d289f53310d8df2f99945c8ea844b0d
JWillian00 <jnmoura.jonathan@braveo.com.br>
2025-02-20 17:44:09 -0300

    img gif msg

--- static/js/consultar.js
+++ static/js/consultar.js
@@ -3,9 +3,11 @@ function consultarChamado() {
     let plataforma = document.getElementById("plataforma-consulta").value;
     let loader = document.getElementById("loader");  
     let consultaDados = document.querySelector(".consulta-dados"); 
+    let comentarioIndicador = document.getElementById("comentario-indicador"); 
 
     loader.style.display = "block";
     consultaDados.style.display = "none";
+    comentarioIndicador.style.display = "none"; // Ocultar o GIF inicialmente
 
     fetch("/consultar", {
         method: "POST",
@@ -22,11 +24,26 @@ function consultarChamado() {
         if (data.error) {
             alert("Erro ao consultar o chamado: " + data.error);
         } else {
-            
+            // Preencher os dados do chamado
             document.getElementById("titulo").innerText = data.titulo;
             document.getElementById("state").innerText = data.estado_chamado;
             document.getElementById("reason").innerText = data.status;
             document.getElementById("board-column").innerText = data.coluna;
+
+            // Verificar se há comentários
+            fetch(`/consultar_comentarios?id_chamado=${idChamado}&plataforma=${plataforma}`)
+                .then(response => response.json())
+                .then(resultado => {
+                    if (resultado.comentarios && resultado.comentarios.length > 0) {
+                        comentarioIndicador.style.display = "block"; // Mostrar o GIF se houver comentários
+                    } else {
+                        comentarioIndicador.style.display = "none"; // Ocultar o GIF se não houver comentários
+                    }
+                })
+                .catch(error => {
+                    console.error("Erro ao buscar comentários:", error);
+                    comentarioIndicador.style.display = "none"; // Ocultar o GIF em caso de erro
+                });
         }
 
         loader.style.display = "none";
